b76eaf6e45ded031e19dbc744439b38bc088f7a6
KyleEgland <kyle.egland@outlook.com>
2019-07-03 09:31:14 -0500

    Updating to handle modal

--- app/main/routes.py
+++ app/main/routes.py
@@ -19,33 +19,79 @@ from urllib.parse import urlparse
 def index():
 
     form = ApiForm()
+
+    # Check for form submission
     if form.validate_on_submit():
+        # Set the session "apiurl" to whatever the contents of the field is
         session['apiurl'] = form.api.data
         try:
+            # Try to parse the url - see if it's a url
             url = urlparse(session['apiurl'])
+            # Try to make a connection to the URL
             socket.gethostbyname(url.netloc)
+            # Given that the connection attempt went ok, set "response" in
+            # session to be True - doing this so that we know to make a request
+            # when the redirect happens
             session['response'] = True
+            # Setting "baseurl" in session to allow some other requests to be
+            # made by jQuery (putting the base url into the template so we
+            # may use it later)
             session['baseurl'] = "{}://{}".format(url.scheme, url.netloc)
+            # Give the user feedback on what is happening
             flash(u'Request sent to: {}'.format(session['apiurl']), 'success')
+            # Redirect so that we can make a request.  Since there isn't a way
+            # (that I've found) to pass a variable back into a view, we're
+            # making the call to the after we've checked the URL
             return redirect(url_for('main.index'))
+        # If we weren't able to connect...
         except socket.gaierror:
+            # Set a "response" variable to False - this will prevent the app
+            # from trying to process a request that can't happen.
             session['response'] = False
+            # Give user feedback
             flash(u'There was an error contacting the server', 'danger')
+
             return redirect(url_for('main.index'))
+    # If this is the first load OR if this route is a redirect...
     elif request.method == 'GET':
+        # We try to get the apiurl from the session...
         try:
+            # Set the form to whatever the last apiurl was
             form.api.data = session['apiurl']
+            # Check for response within the session, if it's true...
             if session['response']:
+                # We make a request to the API - this is a very very simple
+                # request - authentication specified, no headers, data, etc.
                 r = requests.get(session['apiurl'])
+                # Get the json data from the request; grabbing it in this way
+                # (putting it directly into a variable) makes it a dictionary
                 data = r.json()
-                baseurl = session['baseurl']
-                return render_template('index.html', title='Home', form=form,
-                                       apidata=data, baseurl=baseurl)
+                return render_template('index.html',
+                                       # The title of the page, this is a
+                                       # construct that I've created which is
+                                       # handled by the base.html template
+                                       title='Home',
+                                       # Form is used in the template to
+                                       # display the form...yep
+                                       form=form,
+                                       # The information the request returned,
+                                       # used to populate the table
+                                       apidata=data,
+                                       # Used as data within some items in the
+                                       # table to inform jQuery when an item
+                                       # is selected
+                                       baseurl=session['baseurl'])
             else:
                 form.api.data = session['apiurl']
                 return render_template('index.html', title='Home', form=form)
+        # The first time someone visits this page, there will be no apiurl
+        # variable stored in the session, so we handle that with the below
+        # logic.
         except KeyError:
+            # Since this is a test app built for a specific use-case, I've
+            # "hard-coded" the URL to use here
             session['apiurl'] = "https://jsonplaceholder.typicode.com/posts"
+            # Set the form to the apiurl value
             form.api.data = session['apiurl']
 
     return render_template('index.html', tite='Home', form=form)
--- app/static/javascript/main/modal_handler.js
+++ app/static/javascript/main/modal_handler.js
@@ -1,15 +1,43 @@
 /*jshint esversion: 6 */
-$(".usr-id").on('click', function(event) {
-    // Stop the event from propagating to other instances of that class
-    event.stopPropagation();
-    // Stop the event from propagating to children
-    event.stopImmediatePropagation();
-    // I want to use this with hyperlinks - <a></a> - and so I've added
-    // the below line to prevent the link from routing
-    event.preventDefault();
-    // ...the rest of the function...
-    // console.log(event.target.nodeName);
-    // console.log(event.target.data('usrid'));
-    console.log(event.target.href);
-    console.log(event.target.text);
-});
+// Handling User ID selection from table
+// $(".usr-id").on('click', function(event) {
+//     // Stop the event from propagating to other instances of that class
+//     event.stopPropagation();
+//     // Stop the event from propagating to children
+//     event.stopImmediatePropagation();
+//     // I want to use this with hyperlinks - <a></a> - and so I've added
+//     // the below line to prevent the link from routing
+//     event.preventDefault();
+//     // ...the rest of the function...
+//     // console.log(event.target.nodeName);
+//     // console.log(event.target.data('usrid'));
+//     console.log(event.target.href);
+//     console.log(event.target.text);
+// });
+//
+// $(".post-id").on('click', function(event) {
+//     // Stop the event from propagating to other instances of that class
+//     event.stopPropagation();
+//     // Stop the event from propagating to children
+//     event.stopImmediatePropagation();
+//     // I want to use this with hyperlinks - <a></a> - and so I've added
+//     // the below line to prevent the link from routing
+//     event.preventDefault();
+//     // ...the rest of the function...
+//     // console.log(event.target.nodeName);
+//     // console.log(event.target.data('usrid'));
+//     console.log(event.target.href);
+//     console.log(event.target.text);
+// });
+
+$('#infoModal').on('show.bs.modal', function (event) {
+    // Button that triggered the modal
+    let link = $(event.relatedTarget)
+    // Extract info from data-* attributes
+    var recipient = button.data('whatever')
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text('New message to ' + recipient)
+    modal.find('.modal-body input').val(recipient)
+})
